7950762648951e00b4ce27e739528695f289258f
chrisk27 <50341012+chrisk27@users.noreply.github.com>
2022-01-25 11:57:52 -0500

    Generated input field for Iex API Token

--- src/main/java/org/galatea/starter/service/IexClient.java
+++ src/main/java/org/galatea/starter/service/IexClient.java
@@ -1,8 +1,10 @@
 package org.galatea.starter.service;
 
 import java.util.List;
+import javax.websocket.server.PathParam;
 import org.galatea.starter.domain.IexLastTradedPrice;
 import org.galatea.starter.domain.IexSymbol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,14 +16,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "IEX", url = "${spring.rest.iexBasePath}")
 public interface IexClient {
 
-  /**
+   /**
    * Get a list of all stocks supported by IEX. See https://iextrading.com/developer/docs/#symbols.
    * As of July 2019 this returns almost 9,000 symbols, so maybe don't call it in a loop.
    *
    * @return a list of all of the stock symbols supported by IEX.
    */
-  @GetMapping("/ref-data/symbols")
-  List<IexSymbol> getAllSymbols();
+  @GetMapping("/ref-data/iex/symbols")
+  List<IexSymbol> getAllSymbols(@RequestParam("token") String token);
 
   /**
    * Get the last traded price for each stock symbol passed in. See https://iextrading.com/developer/docs/#last.
@@ -30,6 +32,6 @@ public interface IexClient {
    * @return a list of the last traded price for each of the symbols passed in.
    */
   @GetMapping("/tops/last")
-  List<IexLastTradedPrice> getLastTradedPriceForSymbols(@RequestParam("symbols") String[] symbols);
+  List<IexLastTradedPrice> getLastTradedPriceForSymbols(@RequestParam("symbols") String[] symbols, @RequestParam("token") String token);
 
 }
--- src/main/java/org/galatea/starter/service/IexService.java
+++ src/main/java/org/galatea/starter/service/IexService.java
@@ -7,8 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.galatea.starter.domain.IexLastTradedPrice;
 import org.galatea.starter.domain.IexSymbol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * A layer for transformation, aggregation, and business required when retrieving data from IEX.
@@ -27,8 +29,8 @@ public class IexService {
    *
    * @return a list of all Stock Symbols from IEX.
    */
-  public List<IexSymbol> getAllSymbols() {
-    return iexClient.getAllSymbols();
+  public List<IexSymbol> getAllSymbols(@RequestParam("token") String token) {
+    return iexClient.getAllSymbols(token);
   }
 
   /**
@@ -37,11 +39,11 @@ public class IexService {
    * @param symbols the list of symbols to get a last traded price for.
    * @return a list of last traded price objects for each Symbol that is passed in.
    */
-  public List<IexLastTradedPrice> getLastTradedPriceForSymbols(final List<String> symbols) {
+  public List<IexLastTradedPrice> getLastTradedPriceForSymbols(final List<String> symbols, final String token) {
     if (CollectionUtils.isEmpty(symbols)) {
       return Collections.emptyList();
     } else {
-      return iexClient.getLastTradedPriceForSymbols(symbols.toArray(new String[0]));
+      return iexClient.getLastTradedPriceForSymbols(symbols.toArray(new String[0]), token);
     }
   }
 
