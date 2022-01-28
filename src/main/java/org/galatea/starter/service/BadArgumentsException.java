ad3c5cf9cae66526248d7258af447dfa396b0594
chrisk27 <chriskonow27@gmail.com>
2022-01-28 14:17:38 -0500

    Removed parameter dependency in exception handler

--- src/main/java/org/galatea/starter/service/BadArgumentsException.java
+++ src/main/java/org/galatea/starter/service/BadArgumentsException.java
@@ -4,9 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class BadArgumentsException extends RuntimeException{
+public class BadArgumentsException extends RuntimeException {
+  /**
+   * A small error handler specifically for Bad Requests.
+   */
+  public BadArgumentsException(final String message) {
 
-  public BadArgumentsException(String message) {
-    super(message);
   }
 }
